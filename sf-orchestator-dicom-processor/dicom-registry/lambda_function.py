4370bcce3162a97d4801a513e9a06bd4e9e46ccc
Nicolas Cubillos <nicolasdavidcubillos@gmail.com>
2023-09-25 17:11:53 -0500

    ajuste dicom registry

--- sf-orchestator-dicom-processor/dicom-registry/lambda_function.py
+++ sf-orchestator-dicom-processor/dicom-registry/lambda_function.py
@@ -12,14 +12,13 @@ def lambda_handler(event, context):
     try:
         s3 = boto3.client('s3')
         s3.download_file('s3-dicom-processor', key, '/tmp/temp_file.tar.gz')
-
         with tarfile.open('/tmp/temp_file.tar.gz', 'r:gz') as tar:
-            
             if archivo_metadata in tar.getnames():
                 metadata_file = tar.extractfile(archivo_metadata)
                 metadata_content = metadata_file.read()
                 
                 lineas = metadata_content.decode('utf-8').split('\n')
+                
                 payload = {
                     "tipoRegistro": 0,
                     "usuario": 1
@@ -32,16 +31,28 @@ def lambda_handler(event, context):
                         payload["nombrePaciente"] = linea.replace("PatientName: ", "")
                     elif linea.startswith("StudyDescription: "):
                         payload["nombreEstudio"] = linea.replace("StudyDescription: ", "")
-                
+                    elif linea.startswith("Preview: "):
+                        payload["imagenPrevia"] = linea.replace("Preview: ", "")
+                        
                 url = f"{gateway_url}/api-ms-admon/registros"
                 headers = {"Content-Type": "application/json"}
                 response = requests.post(url, json=payload, headers=headers)
-                
-                return {
-                    'statusCode': 200,
-                    'body': metadata_content
-                }
-                
+                if response.status_code == 201:
+                    response_data = response.json()
+                    print('Respuesta del servidor:')
+                    print(response_data)
+                    return {
+                        'statusCode': 200,
+                        'body': metadata_content
+                    }
+                else:
+                    print(f'Error en la solicitud. Código de estado: {response.status_code}')
+                    print('Texto de respuesta del servidor:')
+                    print(response.text)
+                    return {
+                        'statusCode': response.status_code,
+                        'body': response.text
+                    }
             else:
                 return {
                     'statusCode': 404,
