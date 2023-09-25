9548cb7680ebe4dce04b9021e3eebb7338ed434b
esrojas27 <esrojas27@gmail.com>
2023-09-24 20:04:19 -0500

    CORS

--- ms-admon/src/main/java/puj/backend_gui_nodules/config/CorsConfig.java
+++ ms-admon/src/main/java/puj/backend_gui_nodules/config/CorsConfig.java
@@ -1,6 +1,8 @@
 package puj.backend_gui_nodules.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -9,6 +11,7 @@ import org.springframework.web.filter.CorsFilter;
 public class CorsConfig {
 
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOrigin("http://localhost:4200"); // Reemplaza esto con la URL de tu aplicación Angular
